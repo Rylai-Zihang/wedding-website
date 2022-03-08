8fdf0c555c5d9667a50d2b95122042bef97d0ec5
Zihang He <rylaihzh@gmail.com>
2022-03-08 18:57:38 +0800

    fix build bug

--- src/pages/Header.vue
+++ src/pages/Header.vue
@@ -7,7 +7,7 @@
             const header = ref<HTMLElement | null>(null)
 
             onMounted(() => {
-                const height = 1.5 * header.value.clientHeight
+                const height = 1.5 * (header.value?.clientHeight || 0)
                 window.addEventListener("scroll", handleScroll)
                 function handleScroll() {
                     if (window.pageYOffset > height) {
