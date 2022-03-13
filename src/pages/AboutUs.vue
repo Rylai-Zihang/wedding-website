c281790b1c116d72bfdac5e0593bff38ad9bb733
Zihang He <rylaihzh@gmail.com>
2022-03-13 18:09:43 +0800

    add bg-color for figures

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -4,13 +4,13 @@
     >
         <div class="md:w-72 w-56">
             <div class="figure w-full md:h-72 h-56 rounded-full overflow-hidden bg-gray-300">
-                <img class="h-full" src="../assets/pictures/groom@2x.png" />
+                <img class="h-full bg-gray-300" src="../assets/pictures/groom@2x.png" />
             </div>
             <div class="info font-alex text-xl py-8 text-gray-600">🤵🏻：占位符</div>
         </div>
         <div class="md:w-72 w-56">
             <div class="figure w-full md:h-72 h-56 rounded-full overflow-hidden bg-gray-300">
-                <img class="h-full" src="../assets/pictures/bride@2x.png" />
+                <img class="h-full bg-gray-300" src="../assets/pictures/bride@2x.png" />
             </div>
             <div class="info font-alex text-xl py-8 text-gray-600">👰🏻：占位符</div>
         </div>
